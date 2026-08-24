41453fc33285656b1249351faed92156b7185dc5
Rapha-surdo <raphaell.souza@edu.senai.br>
2026-08-24 15:05:30 -0300

    Update - Artistas

---
+++ JS/KamusicArtistas.js
@@ -0,0 +1,39 @@
+const botoesArtistas = [
+    document.getElementById("BB"),
+    document.getElementById("BB1"),
+    document.getElementById("BB2"),
+    document.getElementById("BB3")
+];
+
+const cardsArtistas = [
+    document.getElementById("Opencard"),
+    document.getElementById("Opencard1"),
+    document.getElementById("Opencard2"),
+    document.getElementById("Opencard3")
+];
+
+
+botoesArtistas.forEach(function (botao, indice) {
+
+    botao.addEventListener("change", function () {
+
+        // Fecha todos os conteúdos
+        cardsArtistas.forEach(function (card) {
+            card.classList.remove("aberto");
+        });
+
+        // Desmarca os outros botões
+        botoesArtistas.forEach(function (outroBotao) {
+            if (outroBotao !== botao) {
+                outroBotao.checked = false;
+            }
+        });
+
+        // Se o botão atual estiver marcado, abre o conteúdo correspondente
+        if (botao.checked) {
+            cardsArtistas[indice].classList.add("aberto");
+        }
+
+    });
+
+});
