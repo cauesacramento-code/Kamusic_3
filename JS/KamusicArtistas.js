29aba3f19b7a9a419c32a6105825515d8a687a9a
BlessedGuest <matteo.goncalves@edu.senai.br>
2026-08-25 10:00:54 -0300

    Resolvendo artistas

--- JS/KamusicArtistas.js
+++ JS/KamusicArtistas.js
@@ -2,14 +2,22 @@ const botoesArtistas = [
     document.getElementById("BB"),
     document.getElementById("BB1"),
     document.getElementById("BB2"),
-    document.getElementById("BB3")
+    document.getElementById("BB3"),
+    document.getElementById("BB4"),
+    document.getElementById("BB5"),
+    document.getElementById("BB6"),
+    document.getElementById("BB7")
 ];
 
 const cardsArtistas = [
     document.getElementById("Opencard"),
     document.getElementById("Opencard1"),
     document.getElementById("Opencard2"),
-    document.getElementById("Opencard3")
+    document.getElementById("Opencard3"),
+    document.getElementById("Opencard4"),
+    document.getElementById("Opencard5"),
+    document.getElementById("Opencard6"),
+    document.getElementById("Opencard7")
 ];
 
 
